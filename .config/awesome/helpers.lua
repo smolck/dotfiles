4f8c745c3362c3ddc05766772dcaba3f801edd93
smolck <46855713+smolck@users.noreply.github.com>
2019-07-19 21:29:10 -0500

    Add config for awesomewm

---
+++ .config/awesome/helpers.lua
@@ -0,0 +1,25 @@
+-- NOT MY WORK! Credit goes to github user 'elenapan'.
+-- Here's his/her dotfiles (from whence this was gotten):
+-- https://github.com/elenapan/dotfiles
+
+local gears = require("gears")
+
+local helpers = {}
+
+-- Create rounded rectangle shape
+helpers.rrect = function(radius)
+    return function(cr, width, height)
+        gears.shape.rounded_rect(cr, width, height, radius)
+        --gears.shape.octogon(cr, width, height, radius)
+        --gears.shape.rounded_bar(cr, width, height)
+    end
+end
+
+-- Create rectangle shape
+helpers.rect = function()
+    return function(cr, width, height)
+        gears.shape.rectangle(cr, width, height)
+    end
+end
+
+return helpers
