b26acf73c135f96a6dff67d43519ffff6c4cfc80
smolck <46855713+smolck@users.noreply.github.com>
2019-11-30 16:27:15 -0600

    Finish migration from init.vim -> init.lua
    Lua `plug` implementation courtesy of @norcalli (thanks again!)

---
+++ .config/nvim/lua/helpers.lua
@@ -0,0 +1,141 @@
+local helpers = {}
+local vim = vim
+
+-- Taken from
+-- https://github.com/norcalli/nvim_utils/blob/master/lua/nvim_utils.lua
+local valid_modes = {
+	n = 'n'; v = 'v'; x = 'x'; i = 'i';
+	o = 'o'; t = 't'; c = 'c'; s = 's';
+	-- :map! and :map
+	['!'] = '!'; [' '] = '';
+}
+
+
+LUA_MAPPING = {}
+
+local function escape_keymap(key)
+	-- Prepend with a letter so it can be used as a dictionary key
+	return 'k'..key:gsub('.', string.byte)
+end
+
+function helpers.nvim_apply_mappings(mappings, default_options)
+	-- May or may not be used.
+	local current_bufnr = vim.api.nvim_get_current_buf()
+	for key, options in pairs(mappings) do
+		options = vim.tbl_extend("keep", options, default_options or {})
+		local bufnr = current_bufnr
+		-- TODO allow passing bufnr through options.buffer?
+		-- protect against specifying 0, since it denotes current buffer in api by convention
+		if type(options.buffer) == 'number' and options.buffer ~= 0 then
+			bufnr = options.buffer
+		end
+		local mode, mapping = key:match("^(.)(.+)$")
+		assert(mode, "nvim_apply_mappings: invalid mode specified for keymapping "..key)
+		assert(valid_modes[mode], "nvim_apply_mappings: invalid mode specified for keymapping. mode="..mode)
+		mode = valid_modes[mode]
+		local rhs = options[1]
+		-- Remove this because we're going to pass it straight to nvim_set_keymap
+		options[1] = nil
+		if type(rhs) == 'function' then
+			-- Use a value that won't be misinterpreted below since special keys
+			-- like <CR> can be in key, and escaping those isn't easy.
+			local escaped = escape_keymap(key)
+			local key_mapping
+			if options.dot_repeat then
+				local key_function = rhs
+				rhs = function()
+					key_function()
+					-- -- local repeat_expr = key_mapping
+					-- local repeat_expr = mapping
+					-- repeat_expr = vim.api.nvim_replace_termcodes(repeat_expr, true, true, true)
+					-- nvim.fn["repeat#set"](repeat_expr, nvim.v.count)
+					nvim.fn["repeat#set"](nvim.replace_termcodes(key_mapping, true, true, true), nvim.v.count)
+				end
+				options.dot_repeat = nil
+			end
+			if options.buffer then
+				-- Initialize and establish cleanup
+				if not LUA_BUFFER_MAPPING[bufnr] then
+					LUA_BUFFER_MAPPING[bufnr] = {}
+					-- Clean up our resources.
+					vim.api.nvim_buf_attach(bufnr, false, {
+						on_detach = function()
+							LUA_BUFFER_MAPPING[bufnr] = nil
+						end
+					})
+				end
+				LUA_BUFFER_MAPPING[bufnr][escaped] = rhs
+				-- TODO HACK figure out why <Cmd> doesn't work in visual mode.
+				if mode == "x" or mode == "v" then
+					key_mapping = (":<C-u>lua LUA_BUFFER_MAPPING[%d].%s()<CR>"):format(bufnr, escaped)
+				else
+					key_mapping = ("<Cmd>lua LUA_BUFFER_MAPPING[%d].%s()<CR>"):format(bufnr, escaped)
+				end
+			else
+				LUA_MAPPING[escaped] = rhs
+				-- TODO HACK figure out why <Cmd> doesn't work in visual mode.
+				if mode == "x" or mode == "v" then
+					key_mapping = (":<C-u>lua LUA_MAPPING.%s()<CR>"):format(escaped)
+				else
+					key_mapping = ("<Cmd>lua LUA_MAPPING.%s()<CR>"):format(escaped)
+				end
+			end
+			rhs = key_mapping
+			options.noremap = true
+			options.silent = true
+		end
+		if options.buffer then
+			options.buffer = nil
+			vim.api.nvim_buf_set_keymap(bufnr, mode, mapping, rhs, options)
+		else
+			vim.api.nvim_set_keymap(mode, mapping, rhs, options)
+		end
+	end
+end
+
+-- Taken from
+-- https://github.com/norcalli/nvim_utils/blob/master/lua/nvim_utils.lua
+function helpers.nvim_create_augroups(definitions)
+	for group_name, definition in pairs(definitions) do
+		vim.api.nvim_command('augroup ' .. group_name)
+		vim.api.nvim_command('autocmd!')
+		for _, def in ipairs(definition) do
+			local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+			vim.api.nvim_command(command)
+		end
+		vim.api.nvim_command('augroup END')
+	end
+end
+
+-- Courtesy of @norcalli
+function helpers.plug(path, config)
+  vim.validate {
+    path = {path, 's'};
+    config = {config, vim.tbl_islist, 'an array of packages'};
+  }
+  vim.fn["plug#begin"](path)
+  for _, v in ipairs(config) do
+    if type(v) == 'string' then
+      vim.fn["plug#"](v)
+    elseif type(v) == 'table' then
+      local p = v[1]
+      assert(p, 'Must specify package as first index.')
+      v[1] = nil
+      vim.fn["plug#"](p, v)
+      v[1] = p
+    end
+  end
+  vim.fn["plug#end"]()
+  vim._update_package_paths()
+end
+
+helpers.nvim_options = setmetatable({}, {
+    __index = function(_, k)
+			return vim.api.nvim_get_option(k)
+		end;
+    __newindex = function(_, k, v)
+			return vim.api.nvim_set_option(k, v)
+    end
+  });
+
+return helpers
---
+++ .config/nvim/lua/init.lua
@@ -0,0 +1,407 @@
+local vim                  = vim
+local os                   = require 'os'
+local helpers              = require 'helpers'
+local nvim_options         = helpers.nvim_options
+local nvim_apply_mappings  = helpers.nvim_apply_mappings
+local nvim_create_augroups = helpers.nvim_create_augroups
+local plug                 = helpers.plug
+
+-- Vim function definitions {{{
+vim.fn['BuildComposer'] = function(info)
+    local has = vim.fn.has
+
+    if info.status ~= 'unchanged' or info.force then
+        if has('nvim') then
+            vim.api.command('!cargo build --release')
+        else
+            vim.api.command('!cargo build --release --no-default-features --features json-rpc')
+        end
+    end
+end
+
+vim.fn['LightlineReadonly'] = function()
+    if vim.g.readonly then
+        return ''
+    else
+        return ''
+    end
+end
+
+vim.api.nvim_command [[
+    function! DeleteTrailingWS()
+      exe 'normal mz'
+      %s/\s\+$//ge
+      exe 'normal `z'
+    endfunction
+]]
+
+vim.api.nvim_command [[
+    function! SynStack()
+        if !exists('*synstack')
+            return
+        endif
+        echo map(synstack(line('.'), col('.')), 'synIDattr(v:val, "name")')
+    endfunction
+]]
+
+
+vim.api.nvim_command [[
+    function! LightlineFugitive()
+       if exists('*fugitive#head')
+        let branch = fugitive#head()
+        return branch !=# '' ? ''.branch : ''
+       endif
+       return ''
+    endfunction
+]]
+-- }}}
+
+-- Plugins {{{
+plug('~/.config/nvim/plugged', {
+  'neovim/nvim-lsp',
+
+  'tpope/vim-repeat',
+
+  'dart-lang/dart-vim-plugin',
+
+  'gruvbox-community/gruvbox',
+  'joshdick/onedark.vim',
+
+
+  'bakpakin/fennel.vim',
+  {'jaawerth/fennel-nvim', branch = 'dev'},
+
+  'ncm2/ncm2-cssomni',
+  {'ncm2/ncm2-tern', ['do'] = 'npm install'},
+  'clojure-vim/async-clj-omni',
+  'tpope/vim-fireplace',
+
+  'shlomif/vim-extract-variable',
+
+  -- Floating-window-utilizing plugins.
+  'rhysd/git-messenger.vim',
+  'voldikss/vim-floaterm',
+  'ncm2/float-preview.nvim',
+
+  'sbdchd/neoformat',
+
+  'sainnhe/gruvbox-material',
+
+  'ncm2/ncm2',
+  'ncm2/ncm2-bufword',
+  'ncm2/ncm2-path',
+  'roxma/nvim-yarp',
+
+  'Yggdroot/indentLine',
+  'mhinz/vim-startify',
+
+  'itchyny/lightline.vim',
+  'taohexxx/lightline-buffer',
+
+  'sheerun/vim-polyglot',
+
+  'ntpeters/vim-better-whitespace',
+  'ctrlpvim/ctrlp.vim',
+
+  'kassio/neoterm',
+
+  'neomake/neomake',
+
+  'airblade/vim-gitgutter',
+  'tpope/vim-fugitive',
+  'tpope/vim-surround',
+  'vimwiki/vimwiki',
+})
+-- }}}
+
+vim.api.nvim_command("call neomake#configure#automake('nrwi', 500)")
+
+-- Config for LSPs {{{
+local nvim_lsp  = require 'nvim_lsp'
+local util      = require 'nvim_lsp/util'
+local skeleton  = require 'nvim_lsp/skeleton'
+
+local function init_lsps()
+    nvim_lsp.pyls.setup { {} }
+    nvim_lsp.rls.setup {
+        cmd = { 'rustup', 'run', 'nightly', 'rls' }
+    }
+    nvim_lsp.clangd.setup { {} }
+    nvim_lsp.gopls.setup { {} }
+    nvim_lsp.tsserver.setup { {} }
+
+    skeleton.dart_analyzer = {
+        default_config = {
+            cmd = { 'dart', '/opt/dart-sdk-dev/bin/snapshots/analysis_server.dart.snapshot', '--lsp' };
+            filetypes = { 'dart' };
+            root_dir = util.root_pattern('pubspec.yaml');
+            log_level = vim.lsp.protocol.MessageType.Warning;
+            settings = {};
+        }
+    }
+
+    nvim_lsp.dart_analyzer.setup {
+        on_attach = language_client_setup;
+    }
+end
+-- }}}
+
+-- Globals {{{
+local function init_globals()
+    vim.g.startify_custom_header_quotes             = {{ os.getenv('VERSEOFDAY') }}
+    vim.g.polyglot_disabled                         = { 'dart' }
+    vim.g.floaterm_position                         = 'center'
+    vim.g.floaterm_width                            = nvim_options.columns / 2
+    vim.g.mapleader                                 = ';'
+
+    -- Colorscheme
+    vim.g.colors_name                               = 'gruvbox-material'
+
+    vim.g.neomake_cpp_enabled_makers                = {'clangd'}
+    vim.g.python_highlight_all                      = 1
+
+    vim.g.strip_whitespace_on_save                  = 1
+    vim.g.strip_max_file_size                       = 1000
+    vim.g.strip_whitespace_confirm                  = 0
+end
+-- }}}
+
+-- Lightline config {{{
+local function init_lightline()
+    vim.g.lightline = {
+        colorscheme = 'gruvbox_material';
+        tabline = {
+          left = { { 'bufferinfo' },
+                    { 'separator' },
+                    { 'bufferbefore', 'buffercurrent', 'bufferafter' }, },
+          right = { { 'tabs', 'close' }, },
+        };
+          active = {
+            left = { { 'mode', 'paste' },
+                    { 'fugitive', 'readonly', 'filename', 'modified' }
+            }
+        };
+        component_expand = {
+          buffercurrent = 'lightline#buffer#buffercurrent',
+          bufferbefore = 'lightline#buffer#bufferbefore',
+          bufferafter = 'lightline#buffer#bufferafter',
+        };
+        component_type = {
+          buffercurrent = 'tabsel',
+          bufferbefore = 'raw',
+          bufferafter = 'raw',
+        };
+        component_function = {
+          bufferinfo = 'lightline#buffer#bufferinfo',
+          readonly = 'LightlineReadonly',
+          fugitive = 'LightlineFugitive'
+        };
+        separator = { left = '', right = '' };
+        subseparator = { left = '', right = '' };
+    }
+    -- Lightline-buffer UI settings
+    vim.g.lightline_buffer_logo                     = ' '
+    vim.g.lightline_buffer_readonly_icon            = ''
+    vim.g.lightline_buffer_modified_icon            = '✭'
+    vim.g.lightline_buffer_git_icon                 = ' '
+    vim.g.lightline_buffer_ellipsis_icon            = '..'
+    vim.g.lightline_buffer_expand_left_icon         = '◀ '
+    vim.g.lightline_buffer_expand_right_icon        = ' ▶'
+    vim.g.lightline_buffer_active_buffer_left_icon  = ''
+    vim.g.lightline_buffer_active_buffer_right_icon = ''
+    vim.g.lightline_buffer_separator_icon           = '  '
+
+    -- Requires <https://github.com/ryanoasis/vim-devicons>
+    vim.g.lightline_buffer_enable_devicons          = 1
+
+    vim.g.lightline_buffer_show_bfnr                = 1
+
+    -- See `:help filename-modifiers`
+    vim.g.lightline_buffer_fname_mod                = ':t'
+
+    -- Hide buffer list
+    vim.g.lightline_buffer_excludes                 = { 'vimfiler' }
+
+    vim.g.lightline_buffer_maxflen                  = 30 -- Max file name length
+    vim.g.lightline_buffer_minflen                  = 16 -- Min file name length
+
+    vim.g.lightline_buffer_maxfextlen               = 30 -- Max file extension length
+    vim.g.lightline_buffer_minfextlen               = 3  -- Min file extension length
+
+    -- Reserve length for other componenet (e.g. info, close)
+    vim.g.lightline_buffer_reservelen               = 20
+end
+
+-- }}}
+
+-- Options {{{
+local function init_options()
+    local options = {
+      foldmethod         = 'marker';
+
+      textwidth          = 80;
+      splitbelow         = true;
+      splitright         = true;
+      title              = true;
+      ignorecase         = true;
+      encoding           = 'UTF-8';
+      background         = 'dark';
+      path               = nvim_options.path .. ',' .. vim.api.nvim_call_function('getenv', { 'PWD' });
+      wildmenu           = true;
+      tabstop            = 4;
+      shiftwidth         = 4;
+      softtabstop        = 4;
+      expandtab          = true;
+      smarttab           = true;
+      autoindent         = true;
+
+      number             = true;
+      relativenumber     = true;
+      autochdir          = true;
+      hlsearch           = true;
+      smartcase          = true;
+      cursorline         = true;
+      linebreak          = true;
+      wrap               = false;
+      lazyredraw         = true;
+
+      termguicolors	     = true;
+
+      guifont		     = 'Iosevka\\ Extrabold:h16';
+
+      hidden             = true;
+      showtabline        = 2; -- Always show tabline.
+      completeopt        = {'menuone', 'noinsert', 'noselect'};
+
+      -- Should be equivalent to `set rtp+=<SHARE_DIR>/merlin/vim` in VimL
+      -- Don't think I even need this anymore....
+      rtp                = nvim_options.rtp .. ',' .. '<SHARE_DIR>/merlin/vim'
+    }
+
+    -- Blink cursor if using GNvim.
+    if vim.fn.exists('g:gnvim') then
+        options.guicursor = nvim_options.guicursor .. ',a:blinkon333'
+    end
+
+    -- for k, v in pairs(options) do nvim_options[k] = v end
+
+    -- TODO(smolck): Temporary until change in vim.api.nvim_set_option
+    -- or addition of a different API function or something.
+    for k, v in pairs(options) do
+        if v == true or v == false then
+            vim.api.nvim_command('set ' .. k)
+        elseif type(v) == 'table' then
+            local values = ''
+            for k2, v2 in pairs(v) do
+                if k2 == 1 then
+                    values = values .. v2
+                else
+                    values = values .. ',' .. v2
+                end
+            end
+            vim.api.nvim_command('set ' .. k .. '=' .. values)
+        else
+            vim.api.nvim_command('set ' .. k .. '=' .. v)
+        end
+    end
+end
+-- }}}
+
+-- Autocmds {{{
+local function create_autocmds()
+    local autocmds = {
+        indentation = {
+            {'FileType', 'javascript', 'setlocal shiftwidth=2'},
+            {'FileType', 'scala',      'setlocal shiftwidth=2'},
+            {'FileType', 'dart',       'setlocal shiftwidth=2'}
+        },
+        omnifunc = {
+            {'Filetype', 'dart,rust,python,go,c,cpp', 'setl omnifunc=lsp#omnifunc'},
+            {'Filetype', 'fennel',                    'setl omnifunc=fnl#omniComplete'},
+            {'Filetype', 'lua',                       'setl omnifunc=fnl#omniCompleteLua'}
+        },
+        ncm2 = {
+            -- Enable ncm2 for all buffers.
+            {'BufEnter', '*', 'call ncm2#enable_for_buffer()'}
+        },
+        general = {
+            {'FocusGained', '*', 'checktime'}
+        }
+    }
+
+    nvim_create_augroups(autocmds)
+end
+-- }}}
+
+-- Keymappings {{{
+
+local function create_mappings()
+    local mappings = {
+        ['n<Leader>git'] = {':GitMessenger<CR>',               noremap = true},
+
+        -- Floaterm mappings
+        ['n<Leader>t'   ] = {':FloatermToggle<CR>',            noremap = true},
+        ['i<Leader>t'   ] = {'<C-\\><C-n>:FloatermToggle<CR>', noremap = true},
+        ['t<Leader>t'   ] = {'<C-\\><C-n>:FloatermToggle<CR>', noremap = true},
+
+        -- Esc out of nvim terminal
+        ['t<Esc>'       ] = {'<C-\\><C-n>',                    noremap = true},
+
+        -- YTC -> 'Yank To Clipboard'
+        ['n<Leader>ytc' ] = {'\"+y',                           noremap = true},
+
+        ['n<Leader>cc'  ] = {function() print('') end,         noremap = true},
+
+        ['n<Up>'        ] = {function() jump(0) end,           noremap = true},
+        ['n<Right>'     ] = {function() move_forward(0) end,   noremap = true},
+        ['n<Left>'      ] = {function() move_backward(0) end,  noremap = true},
+        ['n?'           ] = {':call SynStack()<CR>',           noremap = true},
+
+        -- No arrow keys.
+        ['n<Up>'        ] = {'<nop>',                          noremap = true},
+        ['n<Down>'      ] = {'<nop>',                          noremap = true},
+        ['n<Left>'      ] = {'<nop>',                          noremap = true},
+        ['n<Right>'     ] = {'<nop>',                          noremap = true},
+        ['i<Up>'        ] = {'<nop>',                          noremap = true},
+        ['i<Down>'      ] = {'<nop>',                          noremap = true},
+        ['i<Left>'      ] = {'<nop>',                          noremap = true},
+        ['i<Right>'     ] = {'<nop>',                          noremap = true},
+
+        -- Easy navigation of buffers.
+        -- TODO: Better equivalent to this?
+        -- `map <Leader>n :bn<CR>`
+        -- `map <Leader>p :bp<CR>`
+        -- `map <Leader>d :bd<CR>`
+        ['n<Leader>n'   ] = {':bn<CR>',                        noremap = true},
+        ['n<Leader>p'   ] = {':bp<CR>',                        noremap = true},
+        ['n<Leader>d'   ] = {':bd<CR>',                        noremap = true},
+
+        -- Simple command for opening this config.
+        ['n<Leader>init'] = {":sp<Space>~/.config/nvim/lua/init.lua<CR>", noremap = true},
+
+        -- Delete trailing whitespace.
+        ['n<Leader>w'   ] = {':call DeleteTrailingWS()<CR>',   noremap = true},
+
+        --[[
+        ['t<Leader>api' ] = {
+            ":new|put =map(filter(api_info().functions, '!has_key(v:val,''deprecated_since'')'), 'v:val.name')",
+            noremap = true
+        }
+        --]]
+    }
+
+    nvim_apply_mappings(mappings)
+end
+-- }}}
+
+init_globals()
+init_options()
+init_lsps()
+init_lightline()
+
+-- Should be enabled before autocmds (afaik)
+vim.api.nvim_command('syntax enable')
+vim.api.nvim_command('filetype plugin on')
+
+create_mappings()
+create_autocmds()
